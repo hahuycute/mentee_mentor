49cc5a98245bda50d0727031db373aff3825f3fe
hahuycute <nguyenvietheo2004@gmail.com>
2025-09-13 17:10:27 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/mentee_mentor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mentee_mentor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/src/modules/main_home/presentation/main_home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MainHomePage extends StatefulWidget {
+  const MainHomePage({super.key});
+
+  @override
+  State<MainHomePage> createState() => _MainHomePageState();
+}
+
+class _MainHomePageState extends State<MainHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: const Center(
+        child: Text('Hello, World!'),
+      ),
+    );
+  }
+}
